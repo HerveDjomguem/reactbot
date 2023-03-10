82a0f8bba2b694ac734d7babba92d6772fdb347a
HerveDjomguem <hdjomguem@gmail.com>
2023-03-10 12:19:35 +0100

    r

--- front-end/src/components/chatbot/Card.js
+++ front-end/src/components/chatbot/Card.js
@@ -5,20 +5,21 @@ const Card =(props) => {
     <div style={{ float: 'left', paddingRight:30, width:270}}>
          <div className="card">
         <div className="card-image" style={{width: 240}}>
-             {console.log('props',props)}
-          <img  src={props.playload.fields.image.stringValue} />
-          <span className="card-title">{props.playload.fields.header.stringValue}</span>
+             {console.log('props1',props.payload)}
+             {console.log('props2',props.payload)}  
+          <img  alt={props.payload.fields.header.stringValue} src="front-end/src/components/chatbot/laptop.jpg" />
+          <span className="card-title">{props.payload.fields.header.stringValue}</span>
         </div>
         <div className="card-content">
-           {props.playload.fields.description.stringValue}
-           <p><a href='/'>{props.playload.fields.prix.stringValue}</a></p>
+            { props.payload.fields.description.stringValue }
+           <p><a href='/'>{props.payload.fields.prix.stringValue}</a></p>
         </div>
         <div className="card-action">
-          <a target="_blank" rel='noopener noreferrer' href={props.playload.fields.lien.stringValue}>Regardé maintenant</a>
+          <a target="_blank" rel='noopener noreferrer' href={props.payload.fields.lien.stringValue}>Regardé maintenant</a>
         </div>
       </div>
     </div>
     );
 };
-
+//props.payload.fields.image.stringValue !!!!!!!!!
 export default Card;
